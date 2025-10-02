1def0513e07700219396c4067a2c7eb84e0cca85
VegOwOtenks <vegowotenks@jossco.de>
2025-10-02 17:23:25 +0200

    Circular newtype alias, Enum instance for Circular

--- src/Data/Enum/Circular.hs
+++ src/Data/Enum/Circular.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-|
 Module      : Data.Enum.Circular
 Description : Circular successor & predecessor for bounded enum types
@@ -33,3 +36,57 @@ csucc x | x == maxBound = minBound
 cpred :: (Eq a, Enum a, Bounded a) => a -> a
 cpred x | x == minBound = maxBound
         | otherwise     = pred x
+
+
+-- | Type Alias you can use to express your intent and avoid 'Enum' functions from biting you.
+--
+-- Beware: this alters the behaviour of some functions, producing infinite lists (because of circularity)
+
+newtype Circular a = Circular a
+  deriving (Show, Eq, Ord, Bounded)
+
+instance (Bounded a, Eq a, Enum a) => Enum (Circular a) where
+  succ :: Circular a -> Circular a
+  succ (Circular inner) = Circular $ csucc inner
+
+  pred :: Circular a -> Circular a
+  pred (Circular inner) = Circular $ cpred inner
+
+  toEnum :: Int -> Circular a
+  toEnum index = let
+    enumSize = fromEnum $ maxBound @(Circular a)
+    truncatedIndex = index `mod` enumSize
+    in Circular (toEnum truncatedIndex)
+
+  fromEnum :: Circular a -> Int
+  fromEnum (Circular inner) = fromEnum inner
+
+  enumFromThen :: (Bounded a, Eq a, Enum a) => Circular a -> Circular a -> [Circular a]
+  enumFromThen lower higher = let
+    lowerIndex = fromEnum lower
+    higherIndex = fromEnum higher
+    stepSize = abs $ higherIndex - lowerIndex -- absolute step size: wraps around
+    stepList i = let
+      current   = toEnum i
+      nextIndex = fromEnum current + stepSize
+      in current : stepList nextIndex
+    in stepList lowerIndex
+
+  enumFromTo :: (Bounded a, Eq a, Enum a) => Circular a -> Circular a -> [Circular a]
+  enumFromTo current target = if current == target
+    then []
+    else current : enumFromTo (succ current) target
+
+  enumFromThenTo :: (Bounded a, Eq a, Enum a) => Circular a -> Circular a -> Circular a -> [Circular a]
+  enumFromThenTo lower higher target = let
+    lowerIndex = fromEnum lower
+    higherIndex = fromEnum higher
+    stepSize = abs $ higherIndex - lowerIndex -- absolute step size: wraps around
+    stepListTo i = let
+      current = toEnum i
+      nextIndex = fromEnum current + stepSize
+      in if current == target
+        then []
+        else current : stepListTo nextIndex
+    in stepListTo lowerIndex
+
