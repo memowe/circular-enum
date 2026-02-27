0a870a7487fb7af86a3bcf61605d5b5fef648241
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2026-02-27 11:39:12 +0100

    Add failing tests and improve test structure

--- test/Main.hs
+++ test/Main.hs
@@ -3,15 +3,22 @@ module Main (main) where
 import Data.Enum.Circular
 import Data.Function
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 
-data Direction = N | E | S | W deriving (Show, Eq, Enum, Bounded)
+data Dir = N | E | S | W deriving (Show, Eq, Enum, Bounded)
 
 -- Enumeration of all members of the direction enum
+allDirs :: [Dir]
+allDirs = enumFrom minBound :: [Dir]
 
-allDirs :: [Direction]
-allDirs = enumFrom minBound :: [Direction]
+instance Arbitrary Dir where
+  arbitrary = arbitraryBoundedEnum
 
--- compare the first 2 times 'Direction'-Enum-Size items
+instance Arbitrary a => Arbitrary (Circular a) where
+  arbitrary = Circular <$> arbitrary
+
+-- compare the first 2 times 'Dir'-Enum-Size items
 startShouldBe :: (Show a, Eq a) => [a] -> [a] -> Expectation
 startShouldBe = shouldBe `on` take (length allDirs * 2)
 
@@ -33,37 +40,87 @@ main = hspec $ do
   describe "Circular newtype" $ do
 
     describe "Boundaries" $ do
-      it "North after West" $
-        succ (Circular W) `shouldBe` Circular N
+      it "North after West"   $ succ (Circular W) `shouldBe` Circular N
+      it "West before North"  $ pred (Circular N) `shouldBe` Circular W
+
+    describe "Reversibility" $ do
+      prop "succ o pred = id" $ \dir ->
+        (succ . pred) (Circular dir) `shouldBe` Circular (dir :: Dir)
+      prop "pred o succ = id" $ \dir ->
+        (pred . succ) (Circular dir) `shouldBe` Circular (dir :: Dir)
 
     describe "Compatible with inner Enum instance" $ do
-      it "Successors" $
-        iterate succ (Circular minBound)
-          `startShouldBe` fmap Circular (cycle allDirs)
-      it "Predecessors" $
-        iterate pred (Circular maxBound)
-          `startShouldBe` fmap Circular (cycle $ reverse allDirs)
+      prop "Successors" $ \dir -> dir /= (maxBound :: Dir) ==>
+        succ (Circular dir) `shouldBe` Circular (succ dir)
+      prop "Predecessors" $ \dir -> dir /= (minBound :: Dir) ==>
+        pred (Circular dir) `shouldBe` Circular (pred dir)
 
     describe "Out of Bounds" $ do
-      it "fromEnum Boundary" $
+      let len = length allDirs
+      it "Upper example" $
         toEnum 4 `shouldBe` Circular N
-      it "toEnum Truncation" $
-        fromEnum (toEnum 4 :: Circular Direction) `shouldBe` 0
-      it "toEnum: repeating series" $
-        fmap toEnum [0..] `startShouldBe` fmap Circular (cycle allDirs)
+      it "Lower example" $
+        toEnum (-1) `shouldBe` Circular W
+      prop "Cycle down" $ \n -> n >= fromEnum (maxBound :: Dir) ==>
+        toEnum n `shouldBe` (toEnum (n `mod` len) :: Circular Dir)
+      prop "Cycle up" $ \n -> n < 0 ==>
+        toEnum n `shouldBe` (toEnum (n `mod` len) :: Circular Dir)
+      prop "toEnum Truncation" $ \n ->
+        fromEnum (toEnum n :: Circular Dir) `shouldBe` n `mod` len
+      prop "toEnum: repeating series" $ \n -> n >= 0 ==>
+        let toEnums   = toEnum <$> [0..]
+            cAllDirs  = Circular <$> cycle allDirs
+        in  toEnums !! n `shouldBe` cAllDirs !! n
 
     describe "enum[From][Then][To] circularity" $ do
-      it "Stepped Enum Iteration" $
-        enumFromThen (Circular N) (Circular S)
-          `startShouldBe` cycle (fmap Circular [N, S])
-      it "enumeration Wrapping" $
-        enumFromTo (Circular S) (Circular E)
-          `shouldBe` fmap Circular [S, W, N, E]
-        -- forward iteration
-      it "enumeration stepped wrapping" $
-        enumFromThenTo (Circular N) (Circular S) (Circular E)
-          `startShouldBe` cycle (fmap Circular [N, S])
-        -- produces an infinite list because 'E' can never be reached
-      it "enumFrom infinity" $
-        enumFrom (Circular E)
-          `startShouldBe` cycle (fmap Circular [E, S, W, N])
+
+      describe "Examples" $ do
+        it "Stepped Enum Iteration" $
+          enumFromThen (Circular N) (Circular S)
+            `startShouldBe` cycle (fmap Circular [N, S])
+        it "enumeration Wrapping" $
+          enumFromTo (Circular S) (Circular E)
+            `shouldBe` fmap Circular [S, W, N, E]
+          -- forward iteration
+        it "enumeration stepped wrapping" $
+          enumFromThenTo (Circular N) (Circular S) (Circular E)
+            `startShouldBe` cycle (fmap Circular [N, S])
+          -- produces an infinite list because 'E' can never be reached
+        it "enumFrom infinity" $
+          enumFrom (Circular E)
+            `startShouldBe` cycle (fmap Circular [E, S, W, N])
+
+      describe "General properties" $ do
+
+        describe "enumFromThen" $ do
+          prop "Correct start of enumFromThen: first" $ \cd cd' ->
+            head (enumFromThen cd cd') `shouldBe` (cd :: Circular Dir)
+          prop "Correct start of enumFromThen: second" $ \cd cd' ->
+            enumFromThen cd cd' !! 1 `shouldBe` (cd' :: Circular Dir)
+          prop "Correct step size of enumFromThen" $ \cd n -> n /= 0 ==>
+            let cd' = toEnum (fromEnum (cd :: Circular Dir) + n)
+                ds  = enumFromThen cd cd'
+                shouldBeModLen = shouldBe `on` (`mod` length allDirs)
+            in  forAll (sized $ \s -> choose (0, s)) $ \i ->
+                  let cd1 = ds !! i
+                      cd2 = ds !! (i+1)
+                  in  (fromEnum cd2 - fromEnum cd1) `shouldBeModLen` n
+
+        describe "enumFrom: enumFromThen with succ" $ do
+          prop "Same list starts" $ \cd ->
+            enumFrom cd
+              `startShouldBe` enumFromThen (cd :: Circular Dir) (succ cd)
+
+        describe "enumFromThenTo: finite enumFromThen" $ do
+          prop "Start = end -> singleton" $ \cd cd' -> cd' /= cd ==>
+            enumFromThenTo cd cd' cd `shouldBe` [cd :: Circular Dir]
+          prop "Prefix of enumFromThen" $ \cd cd' target ->
+            enumFromThenTo cd cd' target
+              `shouldStartWith` enumFromThen cd (cd' :: Circular Dir)
+
+        describe "enumFromTo: enumFromThenTo with succ" $ do
+          prop "Ends at target" $ \cd cd' ->
+            last (enumFromTo cd cd') `shouldBe` (cd' :: Circular Int)
+          prop "Same list" $ \cd target ->
+            enumFromTo cd target
+              `shouldBe` enumFromThenTo cd (succ cd :: Circular Dir) target
